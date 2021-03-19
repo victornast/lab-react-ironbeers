e3b7ef70fc48b4ceab8b81ef98fc676f7aee25ee
Victor Nastasa <victornast@gmail.com>
2021-03-19 20:37:23 +0100

    style homepage

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,32 +1,46 @@
 import React from 'react';
 
+import './Home.css';
+
 import { Link } from 'react-router-dom';
 
 function Home() {
   return (
     <nav>
-      <Link to="/beers">
-        <img src="/images/beers.png" alt="All Beers" />
-        <h2>All Beers</h2>
-        <p>
+      <Link to="/beers" className="homepage-action">
+        <img
+          className="homepage-action__image"
+          src="/images/beers.png"
+          alt="All Beers"
+        />
+        <h2 className="homepage-action__title">All Beers</h2>
+        <p className="homepage-action__description">
           Lorem ipsum dolor sit amet consectetur, adipisicing elit. Laboriosam
           esse commodi nihil at odio neque, quo quia impedit dignissimos placeat
           iste odit voluptate animi est fugiat aliquam natus, a excepturi!
         </p>
       </Link>
-      <Link to="/random-beer">
-        <img src="/images/random-beer.png" alt="All Beers" />
-        <h2>Random Beer</h2>
-        <p>
+      <Link to="/random-beer" className="homepage-action">
+        <img
+          src="/images/random-beer.png"
+          alt="All Beers"
+          className="homepage-action__image"
+        />
+        <h2 className="homepage-action__title">Random Beer</h2>
+        <p className="homepage-action__description">
           Lorem ipsum dolor sit amet consectetur, adipisicing elit. Laboriosam
           esse commodi nihil at odio neque, quo quia impedit dignissimos placeat
           iste odit voluptate animi est fugiat aliquam natus, a excepturi!
         </p>
       </Link>
-      <Link to="/new-beer">
-        <img src="/images/new-beer.png" alt="All Beers" />
-        <h2>New Beer</h2>
-        <p>
+      <Link to="/new-beer" className="homepage-action">
+        <img
+          src="/images/new-beer.png"
+          alt="All Beers"
+          className="homepage-action__image"
+        />
+        <h2 className="homepage-action__title">New Beer</h2>
+        <p className="homepage-action__description">
           Lorem ipsum dolor sit amet consectetur, adipisicing elit. Laboriosam
           esse commodi nihil at odio neque, quo quia impedit dignissimos placeat
           iste odit voluptate animi est fugiat aliquam natus, a excepturi!
